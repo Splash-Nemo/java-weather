66a4e22d5725be402ad50af4d4c43a2108cefba1
Splash-Nemo <ap457codes@gmail.com>
2024-12-04 12:36:57 +0530

    Added Files+Built GUI Frame

---
+++ WeatherAppGUI.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class WeatherAppGUI extends JFrame{
+    public WeatherAppGUI(){
+        super("Weather-App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 650);
+
+        setLocationRelativeTo(null);
+        setLayout(null);
+
+        setResizable(false);
+    }
+}
