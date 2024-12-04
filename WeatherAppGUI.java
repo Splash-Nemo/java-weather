6468cb919bec85dfc8a14e87733949f7de3eed9e
Splash-Nemo <ap457codes@gmail.com>
2024-12-04 12:48:22 +0530

    Added search text field

--- WeatherAppGUI.java
+++ WeatherAppGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class WeatherAppGUI extends JFrame{
     public WeatherAppGUI(){
@@ -11,5 +12,20 @@ public class WeatherAppGUI extends JFrame{
         setLayout(null);
 
         setResizable(false);
+
+        addGUIComponents();
+    }
+
+    private void addGUIComponents(){
+        //search field
+        JTextField searchTextField= new JTextField();
+        searchTextField.setBounds(15,25,400,30);
+
+        searchTextField.setFont(new Font("Dialog", Font.PLAIN, 20));
+
+        add(searchTextField);
+
+        //search button
+        
     }
 }
